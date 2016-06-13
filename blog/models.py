18f7ae43116d248cd9157dd2802071991eec1d7e
camila <camila.regia2012@gmail.com>
2016-06-13 14:12:33 -0300

    Mudamos o html do site

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from django.db import models
 from django.utils import timezone
 
@@ -13,9 +14,9 @@ class Post(models.Model):
             blank=True, null=True)
 
 
-def publish(self):
-    self.published_date = timezone.now()
-    self.save()
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
 
-def __str__(self):
-    return self.title
+    def __str__(self):
+        return self.title
